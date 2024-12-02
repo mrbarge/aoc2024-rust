0e9c970e90d0f873f2ba1f7f3e66ec624d8f91dd
Matt Bargenquast <matt.bargenquast@gmail.com>
2024-12-02 21:57:55 +1000

    Day 2 added

---
+++ src/bin/02.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+advent_of_code::solution!(2);
+
+fn read_file_to_int_slices(input: &str) -> Vec<Vec<i32>> {
+    let mut lines: Vec<Vec<i32>> = Vec::new();
+    for line in input.lines() {
+        let numbers: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+        lines.push(numbers);
+    }
+    lines
+}
+
+fn in_range(i: i32, j: i32) -> bool {
+    let diff = (i - j).abs();
+    1 <= diff && diff <= 3
+}
+
+fn line_safe(line: &Vec<i32>) -> bool {
+    let mut dirCount = HashMap::from([(true, 0),(false,0)]);
+    for w in line.windows(2) {
+        if !in_range(w[0], w[1]) {
+            return false;
+        }
+        *dirCount.entry(w[1] > w[0]).or_insert(0) += 1;
+    }
+    !(dirCount.values().all(|&v| v > 0))
+}
+
+fn permissive_line_safe(line: &Vec<i32>) -> bool {
+    for i in 0..line.len() {
+        let mut new_line = Vec::new();
+        new_line.extend_from_slice(&line[..i]);
+        new_line.extend_from_slice(&line[i + 1..]);
+        if line_safe(&new_line) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines = read_file_to_int_slices(input);
+    let mut safe_lines = 0;
+    for line in lines.iter() {
+        if line_safe(line) {
+            safe_lines += 1;
+        }
+    }
+
+    Some(safe_lines)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let lines = read_file_to_int_slices(input);
+    let mut safe_lines = 0;
+    for line in lines.iter() {
+        if permissive_line_safe(line) {
+            safe_lines += 1;
+        }
+    }
+    Some(safe_lines)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4));
+    }
+}
